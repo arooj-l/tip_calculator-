f144113e46df551f4475e04812b8375833a1712c
arooj-l <aliaqat@udel.edu>
2026-02-26 07:23:20 -0500

    Basic working version of tip calculator

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 
 
-bill = float(input("enter the bill amount: "))
-percent_amount = float(input("enter the percent amount: "))
-people_num = int(input("Enter the num of people: "))
+def main():
+    bill = float(input("Enter the bill amount: "))
+    tip_percent = float(input("Enter the tip percentages:" ))
+    people = int(input("Enter the number of people: "))
 
-while True:
- try:
-   tip = bill * (percent_amount/100)
-   total_bill = bill + tip
-   per_person = total_bill/people_num
-   
-   print("your amount to pay per person is: ", per_person)
-   break
- except ValueError:
-    print("Enter the valid number")
-    
+    tip = bill * (tip_percent / 100)
+    total = bill + tip
+    per_person = total / people
+
+    print("Tip amount: ", tip)
+    print("Total bill: ", total)
+    print("Each person should pay: ", per_person)
+
+if __name__ == "__main__":
+    main()
