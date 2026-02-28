a68dc3fb1abc117a20ed15a7530ed9b9d9540749
arooj-l <aliaqat@udel.edu>
2026-02-28 07:05:24 -0500

    error handling included

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
-
-
 def main():
-    bill = float(input("Enter the bill amount: "))
-    tip_percent = float(input("Enter the tip percentages:" ))
-    people = int(input("Enter the number of people: "))
+    while True:
+        try:
+            bill = float(input("Enter the bill amount: "))
+            tip_percent = float(input("Enter the tip percentage: "))
+            people = int(input("Enter the number of people: "))
 
-    tip = bill * (tip_percent / 100)
-    total = bill + tip
-    per_person = total / people
+            if bill < 0:
+                print("Bill amount cannot be negative.\n")
+                continue
 
-    print("Tip amount: ", tip)
-    print("Total bill: ", total)
-    print("Each person should pay: ", per_person)
+            if tip_percent < 0:
+                print("Tip percentage cannot be nagative.\n")
+                continue
+
+            if people <= 0:
+                print("Number of people must be atleast 1,\n")
+                continue
+
+            tip = bill * (tip_percent / 100)
+            total_bill = bill + tip
+            per_person = total_bill / people
+
+            print("\n **** Bill Summary **** ")
+            print(f"Tip amount: ${tip:.2f}")
+            print(f"Total bill: ${total_bill:.2f}")
+            print(f"Each person should pay: ${per_person:.2f}")
+
+            break
+
+        except ValueError:
+            print("Invalid input. Please enter numeric value only.\n")
+
+        except ZeroDivisionError:
+            print("Cannot divide by zero. Number of people must be atleast 1.\n")
 
 if __name__ == "__main__":
     main()
